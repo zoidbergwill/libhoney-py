1435107f23f844c5f558437be9c3ea912d0a4f77
Christine Yen <christine@honeycomb.io>
2017-05-04 02:34:27 -0700

    Make sure PYTHONPATH is set while `pdoc`ing?

--- push_docs.sh
+++ push_docs.sh
@@ -5,8 +5,9 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 # build and commit website files
+python setup.py install
 pip install pdoc
-pdoc libhoney --html --html-dir=./docs
+PYTHONPATH=. pdoc libhoney --html --html-dir=./docs
 
 # Check out orphan gh-pages branch, get it set up correctly
 git checkout --orphan gh-pages
@@ -16,7 +17,7 @@ git mv docs/libhoney/*.html ./
 git add .gitignore
 git clean -fd
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-git push origin gh-pages
 
-#git remote add origin-pages https://${GH_TOKEN}@github.com/honeycombio/libhoney-py.git > /dev/null 2>&1
-#git push --quiet --set-upstream origin-pages gh-pages
+# Pushing via secure GH_TOKEN
+git remote add origin-pages https://${GH_TOKEN}@github.com/honeycombio/libhoney-py.git > /dev/null 2>&1
+git push --force --quiet --set-upstream origin-pages gh-pages
