0d0fcd7883d486506d9eccfd7098b306042d8c1b
William Stewart <zoidbergwill@gmail.com>
2017-08-23 11:37:56 +0200

    Track response time as well

--- libhoney/contrib/django/middleware.py
+++ libhoney/contrib/django/middleware.py
@@ -1,5 +1,7 @@
 import os
 import resource
+import time
+
 import libhoney
 
 
@@ -40,65 +42,15 @@ class HoneyMiddleware(object):
             maxrss_after,
         ])
 
+        request.start_time = time.time()
+
         return None
 
     def process_response(self, request, response):
+        response_time = time.time() - request.start_time
         builder = request.honey_builder
         ev = builder.new_event()
+        ev.add_field('responseTime_ms', response_time * 1000)
         ev.send()
 
         return response
-
-# class HoneyMiddleware(object):
-#     def __init__(self):
-#         libhoney.init(writekey=os.environ["HONEY_WRITE_KEY"],
-#                       dataset=os.environ["HONEY_DATASET"])
-#
-#
-#     def process_request(self, request):
-#         request.start_time = time.time()
-#
-#         return None
-#
-#
-#     def process_response(self, request, response):
-#         response_time = time.time() - request.start_time
-#
-#         libhoney.send_now({
-#             "method": request.method,
-#             "scheme": request.scheme,
-#             "path": request.path,
-#             "query": request.GET,
-#             "isSecure": request.is_secure(),
-#             "isAjax": request.is_ajax(),
-#             "isUserAuthenticated": request.user.is_authenticated(),
-#             "username": request.user.username,
-#             "host": request.get_host(),
-#             "ip": request.META['REMOTE_ADDR'],
-#             "responseTime_ms": response_time * 1000,
-#         })
-#
-#         return response
-#
-# class HoneyMiddleware(object):
-#     def __init__(self):
-#         libhoney.init(writekey=os.environ["HONEY_WRITE_KEY"],
-#                       dataset=os.environ["HONEY_DATASET"])
-#
-#
-#     def process_request(self, request):
-#         libhoney.send_now({
-#             "method": request.method,
-#             "scheme": request.scheme,
-#             "path": request.path,
-#             "query": request.GET,
-#             "isSecure": request.is_secure(),
-#             "isAjax": request.is_ajax(),
-#             "isUserAuthenticated": request.user.is_authenticated(),
-#             "username": request.user.username,
-#             "host": request.get_host(),
-#             "ip": request.META['REMOTE_ADDR'],
-#         })
-#
-#         return None
-#
